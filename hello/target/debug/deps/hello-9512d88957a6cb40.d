809022681026aa65d54c051faf6b719e49e26d19
princewill-dev <eekudep@gmail.com>
2024-02-16 20:37:21 -0500

    restarting rust lesson

---
+++ hello/target/debug/deps/hello-9512d88957a6cb40.d
@@ -0,0 +1,5 @@
+/home/swift/myfiles/projects/rust/lesson-002/hello/target/debug/deps/libhello-9512d88957a6cb40.rmeta: src/main.rs
+
+/home/swift/myfiles/projects/rust/lesson-002/hello/target/debug/deps/hello-9512d88957a6cb40.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-a3dd6406d83462d8.d
@@ -0,0 +1,5 @@
+/home/swift/myfiles/projects/rust/lesson-002/hello/target/debug/deps/libhello-a3dd6406d83462d8.rmeta: src/main.rs
+
+/home/swift/myfiles/projects/rust/lesson-002/hello/target/debug/deps/hello-a3dd6406d83462d8.d: src/main.rs
+
+src/main.rs:
