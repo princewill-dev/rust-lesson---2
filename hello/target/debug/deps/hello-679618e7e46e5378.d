ca99d60a53e9a20e3f57ece9e1651bd89ced1b0c
princewill-dev <eekudep@gmail.com>
2024-02-16 20:40:03 -0500

    Refactor code for improved performance

---
+++ hello/target/debug/deps/hello-679618e7e46e5378.d
@@ -0,0 +1,5 @@
+/home/swift/myfiles/projects/rust/lesson-002/hello/target/debug/deps/hello-679618e7e46e5378: src/main.rs
+
+/home/swift/myfiles/projects/rust/lesson-002/hello/target/debug/deps/hello-679618e7e46e5378.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/swift/myfiles/projects/rust/lesson-002/hello/target/debug/hello: /home/swift/myfiles/projects/rust/lesson-002/hello/src/main.rs
